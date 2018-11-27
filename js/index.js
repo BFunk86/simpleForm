8381aec40db07eaf4675060a78e88edd80716279
Brandon Young <fultoncountycommercialcleaning@gmail.com>
2018-11-27 02:13:09 -0500

    Created method to mask phone input

--- js/index.js
+++ js/index.js
@@ -1,64 +1,28 @@
-$(function () {
-    const keyMap = {
-        49: 1,
-        50: 2,
-        51: 3,
-        52: 4,
-        53: 5,
-        54: 6,
-        55: 7,
-        56: 8,
-        57: 9,
-        48: 0
-    };
-
-    $('#phone').focus(function () {
-        var phone = '';
-        $('#phone').keyup(function (event) {
-            var key = event.which;
-            console.log("key is ", key);
-            console.log("keyMap value is", keyMap[key]);
-            switch (key) {
-                case 48:
-                case 49:
-                case 50:
-                case 51:
-                case 52:
-                case 53:
-                case 54:
-                case 55:
-                case 56:
-                case 57:
-                    if (phone.length === 0) {
-                        console.log("First number input");
-                        phone = "(" + keyMap[key];
-                    } else if (phone.length === 4) {
-                        console.log("Adding closing brackets");
-                        phone += ")" + keyMap[key];
-                    } else if (phone.length === 8) {
-                        console.log("Adding dash");
-                        phone += "-" + keyMap[key];
-                    } else {
-                        console.log("Adding regular number");
-                        phone += key[keyMap];
-                    }
-                    break;
-                default:
-                    console.log("Doing nothing");
-                    // Do nothing if it is non numeric
-                    break;
-            }
-            console.log("phone value is ", phone);
-            $('#phone').val(phone);
-        });
-    })
-});
-
-$(() => {
-    console.log("ready!");
-});
+/**
+ * The mast method takes in the current text input and returns a phone number
+ * formatted String based on the length of the current input.
+ * @param input String: The input to mask
+ * @returns {string}: A phone number in the format (xxx) xxx-xxxx
+ */
+var mask = function (input) {
+    // Split the input string into an array
+    var phoneArr = String(input).split("");
+    if (phoneArr.length === 1) {
+        phoneArr.unshift("(");
+    } else if (phoneArr.length === 4) {
+        phoneArr.push(") ");
+    } else if (phoneArr.length === 9) {
+        phoneArr.push("-");
+    }
+    return String(phoneArr.join(""));
+};
 
 (function (window) {
+
+    $('#phone').keydown(function (event) {
+        var phoneInput = $('#phone');
+        phoneInput.val(mask(phoneInput.val()));
+    });
 
     $('#employee-information').submit(function (event) {
         event.preventDefault();
