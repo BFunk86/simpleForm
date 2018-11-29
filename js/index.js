74a78dc1fe1d1634c6829fbe21e334ffda7b309d
Brandon Young <fultoncountycommercialcleaning@gmail.com>
2018-11-29 01:25:12 -0500

    Added documentation and removed unnecessary files

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 (function (window) {
 
+    var url = "http://fakeurl.com";
+
+    /* HELPER METHODS */
 
     /**
-     * The mast method takes in the current text input and returns a phone number
+     * The mask method takes in the current text input and returns a phone number
      * formatted String based on the length of the current input.
      * @param input String: The input to mask
      * @returns {string}: A phone number in the format (xxx) xxx-xxxx
@@ -21,22 +24,54 @@
         return String(phoneArr.join(""));
     };
 
-    var SubmitForm = function(event) {
-        event.preventDefault();
+    /* CALLBACK METHODS */
 
-    }
-
-    // Handle input into the phone field
-    $('#phone').keydown(function (event) {
+    /**
+     * callback function used to handle the event fired
+     * when input is entered into the phone input box
+     * @param event: The input event passed to the callback
+     */
+    var phoneInput = function (event) {
         var phoneInput = $('#phone');
         phoneInput.val(maskPhoneInput(phoneInput.val()));
-    });
+    };
 
-    // Handle Form Submit
-    $('#employee-information').submit(function (event) {
+    /**
+     * callback used to handle the form submit event
+     * @param event: The submit event passed to the callback
+     */
+    var submitForm = function(event) {
         event.preventDefault();
-        console.log($('#employee-information'));
+        var formData = {
+            name: $('#name').val(),
+            eNumber: $('#eNumber').val(),
+            phone: $('#phone').val(),
+            address: $('#address').val(),
+            city: $('#city').val(),
+            state: $('#state').val(),
+            zip: $('#zip').val(),
+            currentEmployer: $('#current-employer').is(':checked')
+        };
+        $.ajax({
+            url: url,
+            type: 'POST',
+            contentType: 'application/json',
+            data: formData,
+            success: function (result) {
+                console.log(result);
+            },
+            error: function(xhr, status, error) {
+                console.error(error);
+            }
+        })
+    };
 
-        console.log("event is", event);
-    })
+    /* EVENTS */
+
+    // Handle input into the phone field
+    $('#phone').keydown(phoneInput);
+
+    // Handle Form Submit event
+    $('#employee-information').submit(submitForm);
+
 })(window);
