7a18afdffd389a9da0809164c5416cff3bc38d3e
Brandon Young <fultoncountycommercialcleaning@gmail.com>
2018-11-29 00:41:01 -0500

    Added styles for the page

--- js/index.js
+++ js/index.js
@@ -1,29 +1,38 @@
-/**
- * The mast method takes in the current text input and returns a phone number
- * formatted String based on the length of the current input.
- * @param input String: The input to mask
- * @returns {string}: A phone number in the format (xxx) xxx-xxxx
- */
-var mask = function (input) {
-    // Split the input string into an array
-    var phoneArr = String(input).split("");
-    if (phoneArr.length === 1) {
-        phoneArr.unshift("(");
-    } else if (phoneArr.length === 4) {
-        phoneArr.push(") ");
-    } else if (phoneArr.length === 9) {
-        phoneArr.push("-");
-    }
-    return String(phoneArr.join(""));
-};
-
 (function (window) {
 
+
+    /**
+     * The mast method takes in the current text input and returns a phone number
+     * formatted String based on the length of the current input.
+     * @param input String: The input to mask
+     * @returns {string}: A phone number in the format (xxx) xxx-xxxx
+     */
+    var maskPhoneInput = function (input) {
+        var phoneArr = String(input).split("");
+
+        if (phoneArr.length === 1) {
+            phoneArr.unshift("(");
+        } else if (phoneArr.length === 4) {
+            phoneArr.push(") ");
+        } else if (phoneArr.length === 9) {
+            phoneArr.push("-");
+        }
+
+        return String(phoneArr.join(""));
+    };
+
+    var SubmitForm = function(event) {
+        event.preventDefault();
+
+    }
+
+    // Handle input into the phone field
     $('#phone').keydown(function (event) {
         var phoneInput = $('#phone');
-        phoneInput.val(mask(phoneInput.val()));
+        phoneInput.val(maskPhoneInput(phoneInput.val()));
     });
 
+    // Handle Form Submit
     $('#employee-information').submit(function (event) {
         event.preventDefault();
         console.log($('#employee-information'));
